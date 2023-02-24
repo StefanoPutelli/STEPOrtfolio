ee351ca44f4178bb5c83af673066334f3fcbdd90
IlNanoAlto <putellistefano@gmail.com>
2023-02-24 20:51:36 +0100

    finale

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import Typewriter from "./comp/typewriter";
 import gitHub from "./img/github.svg";
+import cv from "./img/cv.png";
 
 
 function App() {
 
-  const [screenResize, setScreenResize] = useState(false);
+  const [screenResize, setScreenResize] = useState(window.innerHeight / window.innerWidth);
   const [frammato, setFrammato] = useState(false);
+  const [clickEnabled, setClickEnabled] = useState(true);
 
   useEffect(() => {
     var canvas = document.querySelector("#canvas")
@@ -14,7 +16,7 @@ function App() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     var config = {
-      particleNumber: 10,
+      particleNumber: 7,
       maxParticleSize: 50,
       maxSpeed: 30,
       colorVariation: 255
@@ -105,12 +107,25 @@ function App() {
       window.requestAnimFrame(frame);
     };
 
-    document.body.addEventListener("click", function (event) {
+    var click = (event) => {
+      if (!clickEnabled) return;
+      setClickEnabled(false);
+      window.setTimeout(() => {
+        setClickEnabled(true);
+      }, 500);
       var x = event.clientX,
         y = event.clientY;
       cleanUpArray();
       initParticles(config.particleNumber, x, y);
-    });
+      frame()
+    }
+
+    function resize() {
+      setScreenResize(window.innerHeight / window.innerWidth);
+    }
+
+    document.body.addEventListener("click", click);
+    window.addEventListener('resize', resize);
 
     // First particle explosion
     initParticles(config.particleNumber);
@@ -118,37 +133,36 @@ function App() {
       frame()
       setFrammato(true)
     }
-
-    window.addEventListener('resize', () => {
-      setScreenResize(!screenResize);
-    });
+    
 
     return () => {
-      window.removeEventListener('resize', () => {
-        setScreenResize(!screenResize);
-      });
+      window.removeEventListener('resize', resize);
+      document.body.removeEventListener("click", click);
     };
 
-  }, [screenResize,frammato]);
+  }, [screenResize, frammato, clickEnabled]);
 
-  useEffect(() => {
-    console.log(document.querySelector("#typewrite"))
-  })
   return (
     <div className="App">
       <canvas id="canvas"></canvas>
-      <div className="absolute top-[10%] w-screen">
+      <div className="absolute top-[10vh] w-screen">
         <div className="m-auto w-screen text-center">
           <div className="h-[20vh] mb-[10vh]">
-            <Typewriter minDelay={10} maxDelay={20} text1="Hi, my name is Stefano." text2="I am a Full-stack Developer" />
+            <Typewriter textSize={screenResize >= 1 ? "3vh" : "5vh"} minDelay={10} maxDelay={20} text1="Hi, my name is Stefano." text2="I am a Full-stack Developer" />
           </div>
         </div>
-        <div className="relative top-[10vh]">
+        <div className="absolute top-[20vh] w-screen">
           <img className="m-auto w-[20vh] cursor-pointer" src={gitHub} alt="My github" onClick={() => {
             window.open("https://github.com/StefanoPutelli/", "_blank");
           }} />
-          <div style={{ display: 'inline-block', color: "white", marginTop: "30px", width: "100%", fontFamily: "Source code pro", fontSize: 15, textAlign: "center" }} dangerouslySetInnerHTML={{ __html: "Check out my github to<br>follow my projects" }} />
-          <div style={{ color: "white", marginTop: "10px", width: "100%", fontFamily: "Source code pro", fontSize: 15, textAlign: "center" }} dangerouslySetInnerHTML={{ __html: "Try to click on the background,<br>something cool will happen..." }} />
+          <div style={{ display: 'inline-block', color: "white", marginTop: "20px", width: "100%", fontFamily: "Source code pro", fontSize: 15, textAlign: "center" }} dangerouslySetInnerHTML={{ __html: "Check out my github to<br>follow my projects" }} />
+          <div className="m-auto w-fit">
+            <img className="inline w-[12vh] mt-5 cursor-pointer" src={cv} alt="CV" onClick={() => {
+              window.open("https://drive.google.com/file/d/1Knr0Q_yJuN6LXZ6u8p_Xy7lwKWCppt-X/view?usp=sharing", "_blank");
+            }} />
+            <div style={{ display: 'inline-block', color: "white", marginTop: "0%", fontFamily: "Source code pro", fontSize: 15, textAlign: "center", alignItems: "center" }} dangerouslySetInnerHTML={{ __html: "Here is my CV" }} />
+          </div>
+          <div style={{ color: "white", marginTop: "40px", width: "100%", fontFamily: "Source code pro", fontSize: 15, textAlign: "center" }} dangerouslySetInnerHTML={{ __html: "Try to click on the background,<br>something cool will happen..." }} />
         </div>
       </div>
     </div>
